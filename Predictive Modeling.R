30c5d07b8f002198c074047e240144b574841ba2
battleblake7 <84210032+battleblake7@users.noreply.github.com>
2021-05-16 18:17:30 -0500

    Add files via upload
    Because I want to get into the marketing field, I decided to do a project surrounding predictive modeling for marketing purposes. So, I downloaded data from a bank, wanting to devise a classification model and predict whether or not a potential customer is a likely buyer before deciding whom to contact. Therefore, my job was to help the firm to build a predictive model using historical data with 4,521 observations.
    
    To start I created a train variable, something that could build the model using 2/3 of the data, and created a test variable that would test and calibrate the model. To build the model, I used R part and a minsplit of 50. I then built a decision tree usins ghte data from the model.
    
    Then using a confusion matrix, I was able to complete my final prediction. I computed the performance measures to test the false positivity rate, false-negative rate, specificity, and sensitivity. I also had to calculate the accuracy of the model. The accuracy I calculated was .88, which is high but not perfect. I could adjust some of the variables earlier on to ensure that that it has at least 90% accuracy.
    
    But with this level of accuracy, I would be able to make informed decisions to the bank with accurate results on who is more likely to buy from the bank.

---
+++ Predictive Modeling.R
@@ -0,0 +1,70 @@
+# Clear the workspace
+rm(list=ls())
+cat("\014")
+
+# 1 (a) read in bank.csv into data.frame
+setwd("/Users/ShujingSun/Dropbox/JSOM_Teaching/2021_Spring/Assignment/hw2/")
+b <- read.csv("bank.csv")
+# examine b
+str(b)  # you should have a data.frame with 4,521 observations and 17 variables
+
+# 1 (b) convert to factors
+b$job <- as.factor(b$job)
+b$marital <- as.factor(b$marital)
+b$education <- as.factor(b$education)
+b$default <- as.factor(b$default)
+b$housing <- as.factor(b$housing)
+b$loan <- as.factor(b$loan)
+b$contact <- as.factor(b$contact)
+b$month <- as.factor(b$month)
+b$campaign <- as.factor(b$campaign)
+b$poutcome <- as.factor(b$poutcome)
+b$y <- as.factor(b$y)
+
+# 1 (c) split into training and test
+set.seed(123) # for reproducible results
+# write the code to create b.train and b.test below
+train <- sample(1:nrow(b),nrow(b)*(2/3))
+b.train <- b[train,]
+b.test <- b[-train,]
+
+
+# start with loading the library
+library(rpart)
+library(rpart.plot)
+
+# 2 (a) build the model using rpart, xval=10, minsplit=50
+fit <- rpart(y ~ ., b.train, 
+            method="class", control=rpart.control(xval=10, minsplit=50)) 
+
+# 2 (b) plot the tree
+rpart.plot(fit, type = 1, extra = 4, main="Classification Tree for Direct Marketing")  
+
+# 2 (c) use b.test to get the confusion matrix 
+# put the confusion matrix in variable cm
+y.pred <- predict(fit, b.test, type="class")
+y.actual <- b.test$y
+cm <- table(y.pred, y.actual)
+cm
+
+# 2 (d) let us define the confusion matrix in our terms
+tn = cm[1,1]
+fn = cm[1,2]
+fp = cm[2,1]
+tp = cm[2,2]
+
+# 2 (e) compute performance measures
+# false positive rate (type I error)
+fp / (fp + tn)
+
+# false negative rate (type II error)
+fn / (fn + tp)
+
+# specificity (true negative rate)
+tn / (tn +fp)
+
+# sensitivity (true positive rate, recall)
+tp / (tp + fn)
+
+# accuracy
+(tp +tn)/(tp + tn +fp +fn)
